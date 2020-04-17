44286ee55fdfde223afc978566a0ce9ed07614e1
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-04-17 19:15:54 +0100

    Remove comments from Main

--- src/calebowusuyianoma/sortalgovisualiser/Main.java
+++ src/calebowusuyianoma/sortalgovisualiser/Main.java
@@ -1,14 +1,7 @@
 package calebowusuyianoma.sortalgovisualiser;
 
 public class Main {
-
     public static void main(String[] args) {
         new MainFrame();
     }
 }
-
-//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//            public void run() {
-//                new Frame();
-//            }
-//        });

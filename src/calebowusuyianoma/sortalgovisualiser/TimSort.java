6909b3b52ad6019cd74c6e33766e170d9223e490
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-05-07 14:26:55 +0100

    Refactor query so it does not change state

--- src/calebowusuyianoma/sortalgovisualiser/TimSort.java
+++ src/calebowusuyianoma/sortalgovisualiser/TimSort.java
@@ -13,41 +13,37 @@ public class TimSort extends Sort {
     private int numberOfElements, insertionSortLoopIndex, right, left, key, keyIndex, sortedElementIndex,
             runSize, mergeStartIndex, mergeEndIndex, previousMergeStartIndex;
 
-    public void sort(ArrayList<Integer> data) {
-        int n = data.size();
-
-        for (int i = 0; i < n; i += minimumRun) {
-            insertionSort.sort(data, i, Math.min(i + minimumRun - 1, n - 1));
-        }
-
-        int size = minimumRun;
-        while (size < n) {
-            for (int start = 0; start < n; start += (size * 2)) {
-                int midpoint = start + size - 1;
-                int end = Math.min(start + (size * 2) - 1, n - 1);
-                mergeSort.merge(data, start, midpoint, end);
-            }
-            size *= 2;
-        }
-    }
-
     public void moveToNextStep(ArrayList<Integer> data) {
-        if (!isRunning() && data.size() > 1) {
+        if (data == null) {
+            throw new IllegalArgumentException("The data should contain at least one element, but it is null");
+        } else if (data.size() <= 1) {
+            setSorted(true);
+        } else if (!isRunning()) {
             setRunning(true);
             numberOfElements = data.size();
             runSize = minimumRun;
             setInsertionSortLoopVariables(data);
             runningInsertionSort = true;
-        } else if (runningInsertionSort && keyLessThanSortedElement(data)) {
-            shiftSortedElementToTheRight(data);
-            if (!keyLessThanSortedElement(data)) {
-                data.set(sortedElementIndex + 1, key);
+        } else if (runningInsertionSort) {
+            if (!shouldInsertKey(data)) {
+                shiftSortedElementToTheRight(data);
+                if (shouldInsertKey(data)) {
+                    data.set(sortedElementIndex + 1, key);
+                } else if (keyIndex + 1 > right) {
+                    insertionSortLoopIndex += minimumRun;
+                }
+            } else if (++keyIndex <= right) {
+                key = data.get(keyIndex);
+                sortedElementIndex = keyIndex - 1;
+
+                if (shouldInsertKey(data)) {
+                    insertionSortLoopIndex += minimumRun;
+                }
+            } else if (insertionSortLoopTerminationExpressionValid()) {
+                runningInsertionSort = false;
+            } else {
+                setInsertionSortLoopVariables(data);
             }
-        } else if (runningInsertionSort && (++keyIndex <= right)) {
-            key = data.get(keyIndex);
-            sortedElementIndex = keyIndex - 1;
-        } else if (runningInsertionSort() && !updatedInsertionSortLoopTerminationExpressionValid()) {
-            setInsertionSortLoopVariables(data);
         } else if (runSize < numberOfElements) {
             if (!inMergeForLoop) {
                 mergeStartIndex = 0;
@@ -81,24 +77,38 @@ public class TimSort extends Sort {
         return Math.min(insertionSortLoopIndex + minimumRun - 1, numberOfElements - 1);
     }
 
-    private boolean keyLessThanSortedElement(ArrayList<Integer> data) {
-        return (sortedElementIndex >= left) && (data.get(sortedElementIndex) > key);
+    private boolean shouldInsertKey(ArrayList<Integer> data) {
+        return (sortedElementIndex < left) || (key >= data.get(sortedElementIndex));
     }
 
     private void shiftSortedElementToTheRight(ArrayList<Integer> data) {
         data.set(sortedElementIndex + 1, data.get(sortedElementIndex));
         sortedElementIndex -= 1;
     }
+    
+    private boolean insertionSortLoopTerminationExpressionValid() {
+        return insertionSortLoopIndex >= numberOfElements - 1;
+    }
 
-    private boolean updatedInsertionSortLoopTerminationExpressionValid() {
-        insertionSortLoopIndex += minimumRun;
-        if (insertionSortLoopIndex >= numberOfElements - 1) {
-            runningInsertionSort = false;
+    public void sort(ArrayList<Integer> data) {
+        int n = data.size();
 
-            return true;
+        for (int i = 0; i < n; i += minimumRun) {
+            insertionSort.sort(data, i, Math.min(i + minimumRun - 1, n - 1));
         }
 
-        return false;
+        int size = minimumRun;
+        while (size < n) {
+            for (int start = 0; start < n; start += (size * 2)) {
+                int midpoint = start + size - 1;
+                int end = Math.min(start + (size * 2) - 1, n - 1);
+                System.out.println("About to merge; low: " + start + "; middle: " + midpoint + "; high: " + end);
+                mergeSort.merge(data, start, midpoint, end);
+                System.out.println("Just merged");
+                System.out.println();
+            }
+            size *= 2;
+        }
     }
 
     public static String getName() {
@@ -125,7 +135,47 @@ public class TimSort extends Sort {
         return mergeEndIndex;
     }
 
-    public boolean runningInsertionSort() {
+    public int getNumberOfElements() {
+        return numberOfElements;
+    }
+
+    public int getMinimumRun() {
+        return minimumRun;
+    }
+
+    public int getRunSize() {
+        return runSize;
+    }
+
+    public int getInsertionSortLoopIndex() {
+        return insertionSortLoopIndex;
+    }
+
+    public int getSortedElementIndex() {
+        return sortedElementIndex;
+    }
+
+    public boolean isRunningInsertionSort() {
         return runningInsertionSort;
+    }
+
+    public void setKeyIndex(int index) {
+        keyIndex = index;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public void setSortedElementIndex(int index) {
+        sortedElementIndex = index;
+    }
+
+    public void setRight(int right) {
+        this.right = right;
+    }
+
+    public void setRunningInsertionSort(boolean running) {
+        runningInsertionSort = running;
     }
 }
